5ea3858330de0e4e6f2063681a9034f306c7ffd3
aserena <aserenakelly@gmail.com>
2023-06-15 18:19:46 +0200

    Implémentation classe Jeu

---
+++ src/Model/Jeu.java
@@ -0,0 +1,46 @@
+package Model;
+
+import Controller.Rules;
+
+import java.util.Scanner;
+
+public class Jeu extends Rules {
+    static private int nbJoueur;
+    static private int rep;
+
+    private static void DemandeNbJoueur(){
+        do{
+            System.out.println("Quel nombre il y'a t'il de joueurs ?");
+            Scanner menu = new Scanner(System.in);
+            nbJoueur = menu.nextInt();
+            if(nbJoueur < 2 || nbJoueur > 10){
+                System.out.println("Nombre de joueur incorrect, rentrez un autre nombre de joueurs");
+            }
+        }while(nbJoueur < 2 || nbJoueur > 10);
+    }
+
+    public static void Menu(){
+        String espace = System.getProperty("line.separator");
+        System.out.println("                    Vous allez commencer le jeu 6 QUI PREND                    "
+                + espace +         "                                                                      "
+                + espace +         "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                + espace +         "                                                                      "
+                + espace +         "   Pour commencer veuillez entrer 1  "
+                + espace +         "                                                                      ");
+        Scanner menu = new Scanner(System.in);
+        rep = menu.nextInt();
+        DemandeNbJoueur();
+    }
+
+
+
+    public static int getNbJoueur() {
+        return nbJoueur;
+    }
+
+    public static int getRep() {
+        return rep;
+    }
+
+
+}
